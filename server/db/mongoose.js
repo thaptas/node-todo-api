12b72ce7a1ea2822833f0fc56c4871c77a556919
Tomasz Haptas <tomasz.haptas@rapidcodefactory.com>
2018-10-18 15:46:27 +0200

    add config for environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const mongodbUri = process.env.MONGODB_URI;
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config.js');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
