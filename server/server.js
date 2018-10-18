51d00b233887311dee70c51d234ba7e919f202ed
Tomasz Haptas <tomasz.haptas@rapidcodefactory.com>
2018-10-18 11:57:24 +0200

    get one todo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mangoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -38,7 +39,28 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+
+  console.log(req.params);
+
+  try {
+    if (!ObjectID.isValid(req.params.id)){
+      throw new Error('Todo ID is invalid');
+    }
+  }
+  catch (e){
+      res.status(404).send(e.message);
+  }
+  Todo.findById(req.params.id).then((todo) => {
+    res.send({
+      todo
+    });
+  }, (e) => {
+    res.status(400).send(e.message);
+  });
+});
 
 module.exports = {app};
 // var newTodo = new Todo({
