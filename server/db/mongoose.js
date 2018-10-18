4f6176546097190ae644667209f08bdbe3bc7eb7
Tomasz Haptas <tomasz.haptas@rapidcodefactory.com>
2018-10-18 13:02:04 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
-
+const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+
+mongoose.connect(mongodbUri, { useNewUrlParser: true });
 
 module.exports = {
   mongoose: mongoose
--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
-
+const port = process.env.PORT || 3000;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log('Started on port ' + port);
 });
 
 app.post('/todos', (req, res) => {
@@ -64,36 +64,3 @@ app.get('/todos/:id', (req, res) => {
 });
 
 module.exports = {app};
-// var newTodo = new Todo({
-//     text: 'Cook dinner',
-//     completed: false,
-//
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved togo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// });
-
-// var otherTodo = new Todo({
-//   text: 'First Validated '
-// });
-
-// otherTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo', e);
-// });
-
-//
-// var user = new User({
-//     name: 'Tomasz',
-//     email: 'tomasz@haptas.pl'
-// });
-//
-// user.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('Something went wrong...');
-// })
